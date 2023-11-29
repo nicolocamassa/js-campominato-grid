5d5ee9cf9d9ea363e7489973342d4e1f6ae35da0
nicolocamassa <camassanicolo03@gmail.com>
2023-11-29 16:16:26 +0100

    [UPDATE] table gen connected to button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function tableGen(){
+    btnNone = document.getElementById('genBtn');
+    btnNone.style.display = 'none';
+
     for(let i = 1; i <= 100; i++){
         const divGen = document.createElement("div");
         document.querySelector(".square-container").appendChild(divGen);
         divGen.classList.add('square', 'square'+i);
-
         document.querySelector(".square"+i).innerHTML = i;
     }
 }
 
-tableGen();
