d7cad13d5f98fa6eafada281203e8feeafd6fdc8
nicolocamassa <camassanicolo03@gmail.com>
2023-11-29 16:11:31 +0100

    [UPDATE] added number in cell

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ function tableGen(){
     for(let i = 1; i <= 100; i++){
         const divGen = document.createElement("div");
         document.querySelector(".square-container").appendChild(divGen);
-        divGen.classList.add('square');
+        divGen.classList.add('square', 'square'+i);
+
+        document.querySelector(".square"+i).innerHTML = i;
     }
 }
 
