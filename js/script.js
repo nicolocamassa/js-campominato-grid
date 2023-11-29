5cc0acd0a7af1453131d1df2f0400808ea7125fe
nicolocamassa <camassanicolo03@gmail.com>
2023-11-29 16:33:48 +0100

    [BONUS] clicked square in console and color change

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-function tableGen(){
+function tableGen() {
     btnNone = document.getElementById('genBtn');
     btnNone.style.display = 'none';
 
-    for(let i = 1; i <= 100; i++){
+    for (let i = 1; i <= 100; i++) {
         const divGen = document.createElement("div");
         document.querySelector(".square-container").appendChild(divGen);
-        divGen.classList.add('square', 'square'+i);
-        document.querySelector(".square"+i).innerHTML = i;
+        divGen.classList.add('square', 'square' + i);
+        const squareNum = document.querySelector(".square" + i);
+        squareNum.innerHTML = i
+
+        divGen.addEventListener('click', function () {
+            this.classList.toggle('bg-primary')
+            console.log(`Quadrato ${squareNum.innerHTML} cliccato`)
+        })
     }
 }
 
